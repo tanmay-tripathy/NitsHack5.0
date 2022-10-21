4ba2158082e951638a71644b1afe48eba0f996a0
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-10-22 05:10:54 +0530

    Auth Setup

---
+++ app/lib/homepage.dart
@@ -0,0 +1,49 @@
+// Home page screen
+
+import 'package:app/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  FirebaseAuth auth = FirebaseAuth.instance;
+                  auth.signOut();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignInScreen()),
+                      (route) => false);
+                },
+                child: const Icon(
+                  Icons.logout,
+                  size: 26.0,
+                ),
+              )),
+        ],
+
+        // on appbar text containing 'GEEKS FOR GEEKS'
+        title: const Text("History"),
+      ),
+      // In body text containing 'Home page ' in center
+      body: const Center(
+        child: Text('Nothing to show here'),
+      ),
+    );
+  }
+}
