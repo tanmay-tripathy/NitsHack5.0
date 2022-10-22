f5e66c7a541302fe29fce6e701e6bee39c4f19ac
tanmay-tripathy <ttripathy17@gmail.com>
2022-10-22 11:43:56 +0530

    added /

--- server/server.js
+++ server/server.js
@@ -10,9 +10,11 @@ app.use(express.urlencoded({extended: false}));
 
 
 const user = require('./routes/client.router');
+const admin = require('./routes/admin.router');
 
+app.use('/',(req,res)=>res.send('Hello world'));
 app.use('/user',user);
-
+app.use('/admin',admin);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{console.log(`listening on http://localhost:${PORT}`)});
